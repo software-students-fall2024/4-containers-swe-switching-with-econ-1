2e9b109c839f75a256259b1acc59160af23d591c
cao-exe <tc3661@nyu.edu>
2024-11-18 13:12:08 -0500

    web app documentation, partial DB handling, html and css update

--- web-app/app.py
+++ web-app/app.py
@@ -1,12 +1,34 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-import pymongo 
-from dotenv import load_dotenv
-from pymongo.server_api import ServerApi
-from bson.objectid import ObjectId
-import pyaudio
+"""
+This module implements a Flask web application that records audio from the microphone,
+saves it to a file, and stores the file in a MongoDB database using GridFS.
+Modules:
+    flask: A micro web framework for Python.
+    pymongo: A Python driver for MongoDB.
+    pyaudio: Provides Python bindings for PortAudio, the cross-platform audio library.
+    wave: Provides a convenient interface to the WAV sound format.
+    threading: Higher-level threading interface.
+    gridfs: A Python library for working with MongoDB GridFS.
+Constants:
+    SAMPLE_RATE (int): The sample rate for audio recording.
+    CHANNELS (int): The number of audio channels.
+    FORMAT: The format for audio recording.
+    CHUNK_SIZE (int): The chunk size for audio recording.
+    OUTPUT_FILENAME (str): The default filename for the recorded audio.
+Functions:
+    record_audio(filename=OUTPUT_FILENAME):
+    store_audio_in_mongodb(filename):
+    create_flask_app():
+"""
+# import os
 import wave
 import threading
+from flask import Flask, render_template, redirect, url_for, jsonify
+import pymongo
+# from dotenv import load_dotenv
+from pymongo.server_api import ServerApi
+# from bson.objectid import ObjectId
+import pyaudio
+import gridfs
 
 # Constants for audio recording
 SAMPLE_RATE = 16000
@@ -18,8 +40,27 @@ OUTPUT_FILENAME = "output.wav"
 # Global flag to control recording
 recording_flag = threading.Event()
 
+uri = 'mongodb://localhost/projectt=4'
+client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
+db = client['audio-analysis']
+fs = gridfs.GridFS(db)
+
 # Function to record audio from the microphone
 def record_audio(filename=OUTPUT_FILENAME):
+    """
+    Records audio from the microphone and saves it to a file.
+    Args:
+        filename (str): The name of the file to save the recorded audio. 
+        Defaults to OUTPUT_FILENAME.
+    The function performs the following steps:
+    1. Initializes the PyAudio object.
+    2. Opens a stream to record audio with the specified format, 
+    channels, sample rate, and chunk size.
+    3. Continuously reads audio data from the stream while the recording_flag is set.
+    4. Stops and closes the stream once recording is finished.
+    5. Saves the recorded audio data to a file in WAV format.
+    6. Stores the audio file in MongoDB using the store_audio_in_mongodb function.
+    """
     p = pyaudio.PyAudio()
 
     print("Recording...")
@@ -46,35 +87,55 @@ def record_audio(filename=OUTPUT_FILENAME):
         wf.setframerate(SAMPLE_RATE)
         wf.writeframes(b''.join(frames))
 
+    # Store the audio file in MongoDB
+    store_audio_in_mongodb(filename)
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key ="KEY"
+def store_audio_in_mongodb(filename):
+    """
+    Stores an audio file in a MongoDB database using GridFS.
 
+    Args:
+        filename (str): The path to the audio file to be stored.
 
-    # test conecctions
-    uri = 'mongodb://localhost/project_4'
-    client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
-    db = client['project_4']
+    Returns:
+        None
+    """
+    with open(filename, 'rb') as f:
+        fs.put(f, filename=filename)
+
+def create_flask_app():
+    """
+    Create and configure the Flask application.
+    This function sets up the Flask app and defines the routes for the web application.
+    Returns:
+        Flask app: The configured Flask application instance.
+    Routes:
+        /: Redirects to the index page.
+        /index: Renders the index.html template.
+        /start: Starts the audio recording process.
+        /stop: Stops the audio recording process.
+    """
+
+    flask_app = Flask(__name__)
+    flask_app.secret_key ="KEY"
 
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
-
     ################### Routes ###################
     # Redirect to the index page
-    @app.route('/')
+    @flask_app.route('/')
     def home():
         return redirect(url_for('index'))
     
-    @app.route('/index', methods=['GET'])
+    @flask_app.route('/index', methods=['GET'])
     def index():
         return render_template('index.html')
     
     # Start the audio recording
-    @app.route('/start', methods=['GET', 'POST'])
+    @flask_app.route('/start', methods=['GET', 'POST'])
     def start():
         global recording_flag
         recording_flag.set()
@@ -82,15 +143,15 @@ def create_app():
         return jsonify({"status": "Audio processing started"})
     
     # Stop the audio recording
-    @app.route('/stop', methods=(['GET', 'POST']))
+    @flask_app.route('/stop', methods=(['GET', 'POST']))
     def stop():
         global recording_flag
         recording_flag.clear()
         return jsonify({"status": "Audio processing finished"})
 
-    return app
+    return flask_app
     
 if __name__ == "__main__":
-    app = create_app()
-    flask_port = 3000
-    app.run(port=flask_port, debug=True)
+    app = create_flask_app()
+    FLASK_PORT = 3000
+    app.run(port=FLASK_PORT, debug=True)
